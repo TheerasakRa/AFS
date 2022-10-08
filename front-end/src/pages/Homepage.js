560e32f267d468cec7e3b218878bcf89f418fb34
Theerasak Rachphonsaen <theerasak.ra.63@ubu.ac.th>
2022-10-08 14:15:22 +0700

    fixed all

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -4,7 +4,7 @@ import Weather from "../data/weather/Weather.js";
 const Homepage = () => {
     return (
             <div className=" ">    
-                    <h1 className='text-4xl mt-5'>Homepage</h1>
+                    <h1 className='text-4xl mt-5'>DASHBOARD</h1>
                     <div className='weather pt-4'>
                     <Weather />
                     </div>
